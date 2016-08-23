161c0c181abcbc81d61b1c200f32754c9c254f2b
viktor <viktor@acklenavenue.com>
2016-08-23 02:49:49 -0600

    trying to make it use config variable for backend url

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     baseUrl: function(){
-        return "http://localhost:8000"
+        return process.env.BACKEND_URL ? process.env.BACKEND_URL : "http://localhost:8000";
     }
 }
