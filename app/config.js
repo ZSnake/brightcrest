586c0e0f5f47e0ac7ba769241d3c4ba8fcf1eda3
viktor <viktor@acklenavenue.com>
2016-09-01 21:35:54 -0600

    configuring base url based on environment var

--- app/config.js
+++ app/config.js
@@ -1,6 +1,10 @@
 module.exports = {
     baseUrl: function(){
-        var isRemote = true; 
-        return isRemote ? "https://ladyluck-backend.herokuapp.com" : "http://localhost:8000";
+        if(process.env.environment === 'dev')
+            return 'https://ladyluck-dev.herokuapp.com'
+        if(process.env.environment === 'production')
+            return 'https://ladyluck-backend.herokuapp.com'
+
+        return 'localhost:8000'
     }
 }
