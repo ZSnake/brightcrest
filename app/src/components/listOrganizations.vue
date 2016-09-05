a1dfd6dd2310bb303b1541f08c881fbd80ce9fab
Cesar <cesar777welch@gmail.com>
2016-09-05 02:25:41 -0600

    Revert "tryrevertcommit"
    This reverts commit 647f10ec468a99309b4c2689ed3e4f471472ded5.

--- app/src/components/listOrganizations.vue
+++ app/src/components/listOrganizations.vue
@@ -51,7 +51,7 @@
             data: function(){
                 return {
                     organizations: [],
-                    act_Organization: {}
+                  
                 }
             },
 
@@ -63,16 +63,7 @@
                         swal('Error', 'Error obteniendo las organizaciones del servidor', 'error');
                     });
                 },
-                returnOrganization: function(organization){
 
-                    
-                         act_Organization = organization;
-
-                         return act_Organization;
-                     
-                    
-                    
-                }
             }
 
         }
