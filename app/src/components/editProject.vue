b095f42eea36c83d6f01830bfaff4103eaae1005
Cesar <cesar777welch@gmail.com>
2016-09-05 02:25:27 -0600

    Revert "Revert "tryRevert""
    This reverts commit b82d217ad752a6e6be3b4073dc01150e57aa0ac9.

---
+++ app/src/components/editProject.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="editProjectModal" class="modal modal-fixed-footer">
+
+</div>
+
+</template>
+<script>
+	var swal = require('sweetalert');
+	var config = require('../../config.js');
+	module.exports = {
+		name: 'editProject',
+		ready: function(){
+			$('select').material_select();
+			console.log(this.props.project);
+		}
+
+		props: {
+			project: Object
+		}
+
+	}
+</script>
+<style>
+	.modal { width: 80% !important  }  /* increase the width as per you desire */
+</style>
--- app/src/components/editProjects.vue
+++
@@ -1,9 +0,0 @@
-<template>
-
-</template>
-<script>
-	
-</script>
-<style>
-	.modal { width: 80% !important  }  /* increase the width as per you desire */
-</style>
