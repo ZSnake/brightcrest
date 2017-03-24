7a74c74b236f08f6ea2af953bc9d3f5ec2b3ea5a
Cesar <cesar.welchez@agilesolutions.com>
2017-03-24 11:57:46 -0600

    cambios de tile

--- app/src/components/mapOrganization.vue
+++ app/src/components/mapOrganization.vue
@@ -270,9 +270,7 @@
 		map = L.map('map').setView([14.70, -86.20], 8);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 			id: 'mapbox.streets'
 		}).addTo(map);
 		for (var i = 0; i < this.organizations.length; i++) {
